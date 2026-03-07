cf57af08f9f3bbf7a21fe77092e0aa60b42ddff4
Your Name <youremail@example.com>
2026-03-07 23:29:01 +0600

    feat: add otp and otpExpire fields to user schema for enhanced authentication

--- src/modules/auth/auth.model.ts
+++ src/modules/auth/auth.model.ts
@@ -1,14 +1,25 @@
 import { Schema, model } from "mongoose";
 import { IUser } from "./auth.interface";
 
-
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
   role: { type: String, enum: ["user", "admin"], default: "user" },
+
+  // 👇 profile picture
+  avatar: {
+    type: String,
+    default: ""
+  },
+
   resetPasswordToken: String,
   resetPasswordExpire: Date,
+
+  otp: String,
+  otpExpire: Date,
+
 }, { timestamps: true });
 
 export const User = model<IUser>("User", userSchema);
