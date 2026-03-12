c8635311ef71df30ca679c69dc5ba99b0a32f2cb
Your Name <youremail@example.com>
2026-03-12 13:42:09 +0600

    recepe route

--- src/modules/Recipes/recipes.route.ts
+++ src/modules/Recipes/recipes.route.ts
@@ -1,11 +1,33 @@
-import { Router } from "express";
-import { createProduct, deleteProduct, getProducts } from "./recipes.controller";
+import express from "express";
+import multer from "multer";
+import {
+  getAllRecipes,
+  getRecipeDetails,
+
+  deleteRecipe,
+  updateRecipe
+} from "../../modules/Recipes/recipes.controller";
+import { adminMiddleware } from "../../middleware/admin.middleware";
 
 
-const router = Router();
+const router = express.Router();
 
-router.post("/", createProduct);
-router.get("/", getProducts);
-router.delete("/:id", deleteProduct);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+const upload = multer({ storage });
 
-export default router;
+// CRUD routes
+router.get("/all/recipes", getAllRecipes);
+router.get("/recipe/:id", getRecipeDetails);
+router.put("/update/:id",upload.single("image"),adminMiddleware, updateRecipe);
+router.delete("/delete/:id",adminMiddleware, deleteRecipe);
+
+
+const recipesRoutes = router;
+export default recipesRoutes;
